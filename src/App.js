1c8a6360bdcea91772cf0f4716de7c433a1bcebc
Marco Pasini <pasini.marcoo@gmail.com>
2023-04-07 09:57:46 +0200

    Aggiunte descrizione attività e pagina info

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import "./App.css";
 
 import Navbar from "./components/Navbar";
 import Homepage from "./components/Homepage";
+import Infopage from "./components/Infopage";
+import React from "react";
 
 function App() {
   return (
@@ -10,6 +12,7 @@ function App() {
       <Navbar />
         <div className="overflow-y-scroll">
         <Homepage></Homepage>
+          <Infopage></Infopage>
         </div>
     </div>
   );
---
+++ src/media/info.js
@@ -0,0 +1,52 @@
+export const features = [
+    {
+        name: 'Progettazione',
+        description:
+            'Progettazione per nuove costruzioni, ristrutturazioni, risanamento conservativo, riqualificazione energetica degli edifici, per opere interne ed esterne: rilievi, restituzione grafica digitale, progetti di massima, redazione capitolati e computi metrici estimativi, progetti esecutivi, particolari costruttivi.',
+        imageSrc: 'https://tailwindui.com/img/ecommerce-images/product-feature-07-detail-01.jpg',
+        imageAlt: 'Progettazioni, ristrutturazioni e risanamento',
+    },
+    {
+        name: 'Pratiche edilizie',
+        description:
+            'Permesso di costruire, valutazione impatto paesaggistico, D.I.A., S.C.I.A., C.I.L.A., autorizzazione sanitaria, recupero sottotetti, direzione lavori, collaudi.',        imageSrc: 'https://tailwindui.com/img/ecommerce-images/product-feature-07-detail-02.jpg',
+        imageAlt: 'Pratiche edilizie.',
+    },
+    {
+        name: 'Direzione lavori',
+        description: 'Direzione dei lavori, collaudi provvisori e definitivi.',
+        imageSrc: 'https://tailwindui.com/',
+        imageAlt: 'Direzione dei lavori',
+    },
+    {
+        name: 'Coordinamento in materia di sicurezza',
+        description: 'Coordinamento nei cantieri temporanei e mobili ai sensi del D.Lgs 81/2008 e del D.Lgs. 106/2009 e s.m.i., in fase di progettazione e di esecuzione delle opere.',
+        imageSrc: 'https://tailwindui.com/',
+        imageAlt: 'Coordinamento sicurezza',
+    },
+    {
+        name: 'Catasto Fabbricati',
+        description: 'Variazioni, cambi d’uso, fusioni e frazionamenti. Aggiornamenti catastali: Docfa, tipi mappali.',
+        imageSrc: 'https://tailwindui.com/',
+        imageAlt: 'Catasto',
+    },
+    {
+        name: 'Certificazioni energetiche',
+        description: 'Certificazioni energetiche degli edifici ai sensi del D.G.R. VIII/5773 del 31 ottobre 2007 e s.m.i.',
+        imageSrc: 'https://tailwindui.com/',
+        imageAlt: 'Certificazioni',
+    },
+    {
+        name: 'Stime immobiliari',
+        description: 'Perizie e stime immobiliari, relazione tecnico-descrittiva con rispettivo valore economico di compravendita e/o di locazione.',
+        imageSrc: 'https://tailwindui.com/',
+        imageAlt: 'Stime immobiliari',
+    },
+    {
+        name: 'Design d\'interni',
+        description: 'Studio lay-out distributivo, scelta finiture e materiali, disegni esecutivi, rapporti con fornitori.',
+        imageSrc: 'https://tailwindui.com/',
+        imageAlt: 'Design interni',
+    }
+]
+
