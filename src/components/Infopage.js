9937a7ec3d0ade6a9afe4aa3ad68087f7d29cddc
Marco Pasini <pasini.marcoo@gmail.com>
2023-04-07 10:10:44 +0200

    aggiunte immagini

--- src/components/Infopage.js
+++ src/components/Infopage.js
@@ -9,13 +9,13 @@ export default function Example() {
     return (
         <div className="bg-white">
             <div className="mx-auto max-w-2xl px-4 py-24 sm:px-6 sm:py-32 lg:max-w-7xl lg:px-8">
-                <div className="mx-auto max-w-3xl text-center">
-                    <h2 className="text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">Protect your device</h2>
-                    <p className="mt-4 text-gray-500">
-                        As a digital creative, your laptop or tablet is at the center of your work. Keep your device safe with a
-                        fabric sleeve that matches in quality and looks.
-                    </p>
-                </div>
+                {/*<div className="mx-auto max-w-3xl text-center">*/}
+                {/*    <h2 className="text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">Protect your device</h2>*/}
+                {/*    <p className="mt-4 text-gray-500">*/}
+                {/*        As a digital creative, your laptop or tablet is at the center of your work. Keep your device safe with a*/}
+                {/*        fabric sleeve that matches in quality and looks.*/}
+                {/*    </p>*/}
+                {/*</div>*/}
 
                 <div className="mt-16 space-y-16">
                     {features.map((feature, featureIdx) => (
--- src/media/info.js
+++ src/media/info.js
@@ -1,51 +1,63 @@
+import catasto from '../img/CATASTO_FABBRICATI_E_TERRENI.jpg'
+import certificazioni from '../img/CERTIFICAZIONI_ENERGETICHE_APE.jpg'
+import coordinamento from '../img/COORDINAMENTO_MATERIA_SICUREZZA.JPG'
+import interni from '../img/DESIGN_INTERNI.JPG'
+import dir_lavori from '../img/DIREZIONE_LAVORI.JPG'
+import pratiche from '../img/PRATICHE_EDILIZIE.JPG'
+import progettazione from '../img/PROGETTAZIONE.JPG'
+import perizie from '../img/STIME_PERIZIE.JPG'
+import logo from '../img/Logo.jpg'
+
 export const features = [
     {
         name: 'Progettazione',
         description:
             'Progettazione per nuove costruzioni, ristrutturazioni, risanamento conservativo, riqualificazione energetica degli edifici, per opere interne ed esterne: rilievi, restituzione grafica digitale, progetti di massima, redazione capitolati e computi metrici estimativi, progetti esecutivi, particolari costruttivi.',
-        imageSrc: 'https://tailwindui.com/img/ecommerce-images/product-feature-07-detail-01.jpg',
+        imageSrc: progettazione,
         imageAlt: 'Progettazioni, ristrutturazioni e risanamento',
     },
     {
         name: 'Pratiche edilizie',
         description:
-            'Permesso di costruire, valutazione impatto paesaggistico, D.I.A., S.C.I.A., C.I.L.A., autorizzazione sanitaria, recupero sottotetti, direzione lavori, collaudi.',        imageSrc: 'https://tailwindui.com/img/ecommerce-images/product-feature-07-detail-02.jpg',
+            'Permesso di costruire, valutazione impatto paesaggistico, D.I.A., S.C.I.A., C.I.L.A., autorizzazione sanitaria, recupero sottotetti, direzione lavori, collaudi.',
+        imageSrc: pratiche,
         imageAlt: 'Pratiche edilizie.',
+
     },
     {
         name: 'Direzione lavori',
         description: 'Direzione dei lavori, collaudi provvisori e definitivi.',
-        imageSrc: 'https://tailwindui.com/',
+        imageSrc: dir_lavori,
         imageAlt: 'Direzione dei lavori',
     },
     {
         name: 'Coordinamento in materia di sicurezza',
         description: 'Coordinamento nei cantieri temporanei e mobili ai sensi del D.Lgs 81/2008 e del D.Lgs. 106/2009 e s.m.i., in fase di progettazione e di esecuzione delle opere.',
-        imageSrc: 'https://tailwindui.com/',
+        imageSrc: coordinamento,
         imageAlt: 'Coordinamento sicurezza',
     },
     {
         name: 'Catasto Fabbricati',
         description: 'Variazioni, cambi d’uso, fusioni e frazionamenti. Aggiornamenti catastali: Docfa, tipi mappali.',
-        imageSrc: 'https://tailwindui.com/',
+        imageSrc: catasto,
         imageAlt: 'Catasto',
     },
     {
         name: 'Certificazioni energetiche',
         description: 'Certificazioni energetiche degli edifici ai sensi del D.G.R. VIII/5773 del 31 ottobre 2007 e s.m.i.',
-        imageSrc: 'https://tailwindui.com/',
+        imageSrc: certificazioni,
         imageAlt: 'Certificazioni',
     },
     {
         name: 'Stime immobiliari',
         description: 'Perizie e stime immobiliari, relazione tecnico-descrittiva con rispettivo valore economico di compravendita e/o di locazione.',
-        imageSrc: 'https://tailwindui.com/',
+        imageSrc: perizie,
         imageAlt: 'Stime immobiliari',
     },
     {
         name: 'Design d\'interni',
         description: 'Studio lay-out distributivo, scelta finiture e materiali, disegni esecutivi, rapporti con fornitori.',
-        imageSrc: 'https://tailwindui.com/',
+        imageSrc: interni,
         imageAlt: 'Design interni',
     }
 ]
